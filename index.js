061565e6d4ae7162430a9da316d62c68f4fffc5c
amachkel <amachkel@gmail.com>
2022-03-22 14:40:12 -0400

    created database and tables, and employeeroutes

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-const inquirer = require('inquirer');
-const cTable = require('console.table');
-const mysql = require('mysql2');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const inquirer = require("inquirer");
---
+++ routes/employeeRoutes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const mysql = require("mysql2");
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: config.DB_USER,
+      // MySQL password
+      password: config.DB_PASSWORD,
+      database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+  );
+
+  module.exports = router;
