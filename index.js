a66b42fd9a7764ff3fb886b93747830ea8676174
amachkel <amachkel@gmail.com>
2022-03-22 17:34:40 -0400

    started inquirer prompt

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 const inquirer = require("inquirer");
+
+getChoice = () => {
+  inquirer.prompt([
+    {
+      type: "checkbox",
+      name: "tables",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "Add Employee",
+        "Update Employee Role",
+        "View All Roles",
+        "Add Role",
+        "View All Departments",
+        "Add Department",
+      ],
+    },
+  ])
+  .then((response) => {
+    console.log(response);
+  })
+};
+getChoice();
