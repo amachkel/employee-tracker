3c9d9039d76fa6441232b696bee0a9b933e93dbe
amachkel <amachkel@gmail.com>
2022-03-22 19:04:58 -0400

    adding functions for tables

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
 const inquirer = require("inquirer");
 
 getChoice = () => {
-  inquirer.prompt([
-    {
-      type: "checkbox",
-      name: "tables",
-      message: "What would you like to do?",
-      choices: [
-        "View All Employees",
-        "Add Employee",
-        "Update Employee Role",
-        "View All Roles",
-        "Add Role",
-        "View All Departments",
-        "Add Department",
-      ],
-    },
-  ])
-  .then((response) => {
-    console.log(response);
-  })
+  inquirer
+    .prompt([
+      {
+        type: "checkbox",
+        name: "tables",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "Add Employee",
+          "Update Employee Role",
+          "View All Roles",
+          "Add Role",
+          "View All Departments",
+          "Add Department",
+        ],
+      },
+    ])
+    .then((response) => {
+      const choice = response.tables[0].split(" ");
+      let action = choice[0];
+      let table = choice[choice.length - 1];
+      console.log(`table: ${table}`);
+      console.log(`action: ${action}`);
+      return action === "View"
+        ? viewTable(table)
+        : action === "Add"
+        ? addToTable(table)
+        : updateTable(table);
+    });
+};
+
+viewTable = (table) => {
+  if (table.slice(-1) === "s") {
+    console.log(`view: ${table}`);
+  }
+};
+
+addToTable = (table) => {
+  console.log(`add to: ${table}`);
+};
+
+updateTable = (table) => {
+  console.log(`update: ${table}`);
 };
 getChoice();
